007e13eb0bd9e4ec19a959711300943107ca29ed
Pedro Arns Junior <pedroarnsjr@outlook.com>
2025-01-20 17:51:40 -0300

    Cap06 Métodos: uma visão mais aprofundada

---
+++ Cap06/src/DrawSmileyTeste.java
@@ -0,0 +1,19 @@
+import java.awt.Component;
+
+//Figura 6.17: DrawSmileyTeste.java
+//Aplicativo de teste que exibe um rosto sorridente.
+import javax.swing.JFrame;
+
+public class DrawSmileyTeste
+{
+    public static void main(String[] args)
+    {
+        DrawSmiley panel = new DrawSmiley();
+        JFrame app = new JFrame();
+
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.add(panel);
+        app.setSize(230, 250);
+        app.setVisible(true);
+    } //fim de main
+} //fim da classe DesenharSmiley
---
+++ Cap06/src/MetodoSobrecargaTeste.java
@@ -0,0 +1,11 @@
+//Figura 6.14: MetodoSobrecargaTeste.java --> traducao do ingles MethodOverlordTest.java
+//Aplicacao para testar a classe MethodSobrecarga.
+
+public class MetodoSobrecargaTeste
+{
+    public static void main(String[] args)
+    {
+        MetodoSobrecarga metodoSobrecarga = new MetodoSobrecarga();
+        metodoSobrecarga.testeMetodosSobrecarregados();
+    } //fim de main
+} //fim da classe MetodoSobrecargaTeste
